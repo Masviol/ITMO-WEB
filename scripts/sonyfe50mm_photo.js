f275474a83403e2b8954b1bc3fdd01ad591ecad7
Masviol <spec4tube@gmail.com>
2023-12-18 09:09:23 +0300

    lab 5-6

---
+++ scripts/sonyfe50mm_photo.js
@@ -0,0 +1,58 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const preloader = document.querySelector('.blockBlack__content__preloader');
+    const content = document.querySelector('.content__photo');
+
+    fetchData()
+        .then(data => {
+            // Step 4: Render the data
+            renderData(data);
+        })
+        .catch(error => {
+            // Step 6: Handle errors
+            handleError(error);
+        })
+        .finally(() => {
+            // Step 2: Hide the preloader
+            preloader.style.display = 'none';
+        });
+
+    function fetchData() { 
+        const randomFilter = Math.random() < 0.5 ? 'id_gte=100' : 'id_gte=200';
+        const apiUrl = `https://jsonplaceholder.typicode.com/photos?${randomFilter}`;
+        return fetch(apiUrl)
+            .then(response => { 
+                if(!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+                return response.json();
+            });
+    }
+
+    function renderData(data) { 
+        content.innerHTML = "";
+    
+        data.some((photo, index) => {
+            const photoElement = document.createElement("img");
+            const element = document.createElement("li")
+            photoElement.src = photo.url;
+            photoElement.alt = photo.title;
+            photoElement.id = photo.id
+            
+
+            taskHTML = `
+                <li>Фото номер: ${photoElement.id}</li>
+            `;
+
+            content.insertAdjacentHTML("beforeend", taskHTML)
+            element.appendChild(photoElement)
+            content.appendChild(element);
+    
+            return index >= 4;
+        });
+    }
+    
+
+    function handleError(error) { 
+        content.innerHTML = `<p class="error-massage">⚠ Something went wrong: ${error.message}</p>`;
+    }
+})
--- scripts/todo.js
+++ scripts/todo.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('#form');
 const input = document.querySelector('#input');
 const todolist = document.querySelector('.todo-list');
+const emptyList = document.querySelector('.todo-list__empty');
 
 let tasks = [];
 
@@ -8,59 +9,100 @@ if (localStorage.getItem('tasks')) {
     tasks = JSON.parse(localStorage.getItem('tasks'));
 }
 
-tasks.forEach(function(task) {
-    const taskHTML = `
-    <div id="${task.id}" class="todo-item">
-        ${task.text}
-        <button type="button" data-action="done">Done</button>
-    </div>
-`;
-todolist.insertAdjacentHTML("beforeend", taskHTML);
+tasks.forEach(function (task) {
+    renderTask(task);
 });
+
+checkEmtyList();
 
 form.addEventListener('submit', addTask);
 
 todolist.addEventListener("click", deleteTask);
 
-function addTask(event) { 
+todolist.addEventListener("click", doneTask);
+
+function addTask(event) {
     event.preventDefault();
     const task = input.value;
     const newTask = {
-        id: Date.now(),
+        id: generateUniqueId(),
         text: task,
+        done: false,
     };
     tasks.push(newTask);
     saveToLocalStorage();
-    const taskHTML = `
-        <div id="${newTask.id}" class="todo-item">
-            ${newTask.text}
-            <button type="button" data-action="done">Done</button>
-        </div>
-    `;
-    todolist.insertAdjacentHTML("beforeend", taskHTML);
+    renderTask(newTask);
     input.value = "";
     input.focus();
+
+    checkEmtyList();
 }
 
 function deleteTask(event) {
-    if (event.target.dataset.action === 'done') { 
-        const parentNode = event.target.closest('.todo-item');
+    if (event.target.dataset.action === 'delete') {
+        let parentNode = null;
+        if (event.target.closest('.todo-item') == null) {
+            parentNode = event.target.closest('.todo-item--done')
+        } else { parentNode = event.target.closest('.todo-item') }
         const id = parentNode.id;
-        const index = tasks.findIndex(function(task) { 
-            if (task.id == id) { 
+        const index = tasks.findIndex(function (task) {
+            if (task.id == id) {
                 return true;
             }
         })
 
-        console.log(index);
-
-        tasks.splice(index, 1)
+        tasks.splice(index, 1);
         saveToLocalStorage();
         parentNode.remove();
     }
+
+    checkEmtyList();
 }
 
-function saveToLocalStorage() { 
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-    console.log("eeee")
+function doneTask() {
+    if (event.target.dataset.action === 'done') {
+        const parentNode = event.target.closest('.todo-item');
+        parentNode.classList.add('todo-item--done');
+
+        const id = Number(parentNode.id);
+        const task = tasks.find(function (task) {
+            if (task.id === id) {
+                return true;
+            }
+        })
+
+        task.done = true;
+        saveToLocalStorage();
+    }
 }
+
+function checkEmtyList() {
+    if (todolist.children.length > 2) {
+        emptyList.classList.add('none')
+    } else emptyList.classList.remove('none')
+}
+
+function saveToLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+function renderTask(task) {
+    const cssClass = task.done ? 'todo-item--done' : 'todo-item'
+    const taskHTML = `
+    <div id="${task.id}" class="${cssClass}">
+        ${task.text}
+        <div class="todo-item__buttons">
+        <button class="buttons__delete_button" type="button" data-action="delete">Удалить</button>
+        <button class="buttons__done_button" type="button" data-action="done">Готово</button>
+        </div>
+    </div>
+`;
+    todolist.insertAdjacentHTML("beforeend", taskHTML);
+}
+
+function generateUniqueId() {
+    const timestampPart = Date.now().toString(36);
+    const randomPart = Math.random().toString(36).substr(2, 5);
+  
+    return `${timestampPart}${randomPart}`;
+  }
